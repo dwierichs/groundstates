33de0c084c7cb25213145b2915338ecb5a4055cd
dwierichs <davidwierichs@gmail.com>
2019-11-13 01:07:33 +0100

    Trying to access energy subclasses. Solve this!

--- core/energy_models.py
+++ core/energy_models.py
@@ -2,15 +2,14 @@ from django.db import models
 
 from .models import Energy
 
-
 class Energy_TFI(Energy):
     h = models.DecimalField( max_digits=8, decimal_places=3, verbose_name='h' )
     J = models.DecimalField( max_digits=8, decimal_places=3, verbose_name='J' )
-    n_par = None
+    #n_par = models.IntegerField( default=0, verbose_name='n_par_Esubclass' )
+
 
     def save(self, *args, **kwargs):
-        self.n_par = len(self.__dict__)-len(super().__dict__)
-        self.n_par = 1
+        #self.n_par = len(dir(self))-len(dir(super(Energy, self)))-len(dir(super(models.Model, self)))
         super().save(*args, **kwargs)
 
 # Always add newly added energy models to the following list in order to have it registered in admin.py
