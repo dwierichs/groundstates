6f5942d2a3711eabb37ba37b66167658a3a1c3ec
David Wierichs <wierichs@l101.thp.uni-koeln.de>
2019-11-14 11:21:23 +0100

    changes?

--- core/energy_models.py
+++ core/energy_models.py
@@ -14,8 +14,19 @@ class Energy_TFI(Energy):
                                     ('J', self.J,),
                                     ]
 
+class Energy_XXX(Energy):
+    
+    h = models.DecimalField( max_digits=14, decimal_places=10 )
+
+    def get_params(self):
+        return super().get_params() + [
+                                    ('h', self.h,),
+                                    ]
+
+
 
 # Always add newly added energy models to the following list in order to have it registered in admin.py
 _AUTO_REGISTER = [
     Energy_TFI,
+    Energy_XXX,
     ]
